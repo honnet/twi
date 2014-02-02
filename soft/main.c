325425bf88ce519d151318fc0378e76b05d9c886
Cedric Honnet <honnet@telecom-paristech.org>
2014-02-01 22:33:17 -0800

    1st TX test: send '0'/'1' every 90ms and blink.

--- soft/main.c
+++ soft/main.c
@@ -1,62 +1,96 @@
-#include "simple_uart.h"
-#include "board.h"
-#include "nrf_delay.h"
+/* Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
+
+/** @file
+*
+* @defgroup nrf_dev_led_radio_tx_example_main main.c
+* @{
+* @ingroup nrf_dev_led_radio_tx_example
+*
+* @brief Radio Transmitter Example Application main file.
+*
+* This file contains the source code for a sample application using the NRF_RADIO peripheral.
+*
+*/
+
+#include <stdint.h>
+#include "radio_config.h"
 #include "nrf_gpio.h"
-#include "twi_master.h"
-#include "mpu6050.h"
+#include "board.h"
 
-static const int DEVADR = 0x68;
+static uint8_t packet[4];  ///< Packet to transmit
 
-#define STRINGIFY(x) #x
-#define STR(x) STRINGIFY(x)
-#pragma message "\n\n>>> TX PIN = " STR(TX_PIN_NUMBER)
-#pragma message "\n\n>>> RX PIN = " STR(RX_PIN_NUMBER)
-#include "twi_master_config.h"
-#pragma message "\n\n>>> SDA PIN = " STR(TWI_MASTER_CONFIG_DATA_PIN_NUMBER)
-#pragma message "\n\n>>> SCL PIN = " STR(TWI_MASTER_CONFIG_CLOCK_PIN_NUMBER)
-
-int main(void)
+void init(void)
 {
-    nrf_gpio_cfg_input(BUTTON, NRF_GPIO_PIN_PULLDOWN);
-    nrf_gpio_cfg_output(LED);
-    nrf_gpio_pin_set(LED);
+  nrf_gpio_cfg_output(LED);
 
-    simple_uart_config(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER, false);
-    simple_uart_putstring((const uint8_t *)" \r\nStart:\r\n");
+  /* Start 16 MHz crystal oscillator */
+  NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_HFCLKSTART = 1;
 
-    // initialize I2C:
-    if (!twi_master_init())
-        for(;; nrf_gpio_pin_toggle(LED), nrf_delay_ms(200));     // quick blink if error
+  /* Wait for the external oscillator to start up */
+  while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
+  {
+  }
 
-    // initialize IMU:
-    nrf_delay_ms(4); // time needed to power up the IMU
-    if (!mpu6050_init(DEVADR))
-        for(;; nrf_gpio_pin_toggle(LED), nrf_delay_ms(600));    // less quick blink if error
+  // Set radio configuration parameters
+  radio_configure();
 
-    while(true) {
-        const uint8_t to = 10; // ms
-        uint8_t cr = '0';
-        if ( simple_uart_get_with_timeout(to, &cr) ) {
-            if (cr >= 'A' && cr <= 'z') {
-                simple_uart_putstring((const uint8_t *)"Received: ");
-                simple_uart_put(cr);
-                simple_uart_putstring((const uint8_t *)"\r\n");
-
-                for (int i=0; i<6; i++) {
-                    nrf_gpio_pin_toggle(LED);
-                    nrf_delay_ms(30);
-                }
-            }
-        }
-
-        if (nrf_gpio_pin_read(BUTTON) ) {
-            simple_uart_putstring((const uint8_t *)"Button pressed.\r\n");
-
-            for (int i=0; i<4; i++) {
-                nrf_gpio_pin_toggle(LED);
-                nrf_delay_ms(90);
-            }
-        }
-    }
+  // Set payload pointer
+  NRF_RADIO->PACKETPTR = (uint32_t)packet;
 }
 
+/**
+ * @brief Function for application main entry.
+ * @return 0. int return type required by ANSI/ISO standard.
+ */
+int main(void)
+{
+  init();
+  nrf_gpio_pin_set(LED);
+
+  char b = 0;
+  while(1)
+  {
+    if (b)
+      nrf_gpio_pin_set(LED);
+    else
+      nrf_gpio_pin_clear(LED);
+
+    // Place the read character in the payload, enable the radio and
+    // send the packet:
+    packet[0] = b ? '1' : '0';
+    NRF_RADIO->EVENTS_READY = 0U;
+    NRF_RADIO->TASKS_TXEN = 1;
+    while (NRF_RADIO->EVENTS_READY == 0U)
+    {
+    }
+    NRF_RADIO->TASKS_START = 1U;
+    NRF_RADIO->EVENTS_END = 0U;
+    while(NRF_RADIO->EVENTS_END == 0U)
+    {
+    }
+    NRF_RADIO->EVENTS_DISABLED = 0U;
+    // Disable radio
+    NRF_RADIO->TASKS_DISABLE = 1U;
+    while(NRF_RADIO->EVENTS_DISABLED == 0U)
+    {
+    }
+
+    b = !b;
+    nrf_delay_ms(90);
+  }
+}
+
+/**
+ *@}
+ **/
