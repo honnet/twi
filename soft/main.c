3b4c7bbd8ead4cd78209a9c3233512296f537ae2
Cedric Honnet <honnet@telecom-paristech.org>
2014-03-27 14:03:19 -0700

    Print compass heading.

--- soft/main.c
+++ soft/main.c
@@ -1,71 +1,39 @@
-/* Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
- */
-
-/** @file
-*
-* @defgroup nrf_dev_led_radio_tx_example_main main.c
-* @{
-* @ingroup nrf_dev_led_radio_tx_example
-*
-* @brief Radio Transmitter Example Application main file.
-*
-* This file contains the source code for a sample application using the NRF_RADIO peripheral.
-*
-*/
-
-#include <stdlib.h> // sprintf()
 #include <stdio.h>
 #include <stdint.h>
-#include "radio_config.h"
+#include <math.h>
 #include "nrf_gpio.h"
 #include "board.h"
 #include "nrf_delay.h"
-#include "simple_uart.h"
 #include "MPU9150.h"
 
-static uint8_t packet[4];  ///< Packet to transmit
+void simple_uart_print_int(int i);
+
+#define DEBUG_PRINT
+
+#ifdef DEBUG_PRINT
+#  include <stdlib.h> // sprintf()
+#  include "simple_uart.h"
+#  define DP_cnf(...) simple_uart_config(__VA_ARGS__)
+#  define DP_int(x)   simple_uart_print_int(x)
+#  define DP_str(x)   simple_uart_putstring((const uint8_t *)(x))
+#else
+#  define DP_cnf(...)
+#  define DP_int(x)
+#  define DP_str(x)
+#endif
 
 MPU9150 accelGyroMag;
 int16_t a[3], g[3], m[3];
 
 void init(void)
 {
-  nrf_gpio_cfg_output(LED);
-  simple_uart_config(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER, false);
-  accelGyroMag.initialize();
-  simple_uart_putstring( accelGyroMag.testConnection() ?
-                        (const uint8_t *)"MPU9150 connection successful\r\n" :
-                        (const uint8_t *)"MPU9150 connection failed\r\n" );
-
-///* Start 16 MHz crystal oscillator */
-//NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-//NRF_CLOCK->TASKS_HFCLKSTART = 1;
-
-///* Wait for the external oscillator to start up */
-//while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
-//{
-//}
-
-//// Set radio configuration parameters
-//radio_configure();
-
-//// Set payload pointer
-//NRF_RADIO->PACKETPTR = (uint32_t)packet;
+    nrf_gpio_cfg_output(LED);
+    DP_cnf(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER, false);
+    accelGyroMag.initialize();
+    DP_str( accelGyroMag.testConnection() ?
+            "MPU9150 connection OK\r\n" :
+            "MPU9150 connection failed\r\n" );
 }
-
-/**
- * @brief Function for application main entry.
- * @return 0. int return type required by ANSI/ISO standard.
- */
 
 int main(void)
 {
@@ -74,59 +42,39 @@ int main(void)
 
     while(1)
     {
-        int16_t acc = accelGyroMag.getAccelerationZ();
-
-        char buf[8]; // max = 2**16 = 65536
-        sprintf(buf, "%d\t", acc);
-        simple_uart_putstring((const uint8_t *)buf);
-
-        for (int i=0; i<abs(acc)>>8; i++)
-            simple_uart_put('+');
-        simple_uart_put('\n');
-
-/*
         accelGyroMag.getMotion9(&a[0], &a[1], &a[2], &g[0], &g[1], &g[2], &m[0], &m[1], &m[2]);
-        for (int i=0; i<3; i++) {
-            sprintf(buf, "%d\t" , a[i]);
-            simple_uart_putstring((const uint8_t *)buf);
-        }
-        for (int i=0; i<3; i++) {
-            sprintf(buf, "%d\t" , g[i]);
-            simple_uart_putstring((const uint8_t *)buf);
-        }
-        for (int i=0; i<3; i++) {
-            sprintf(buf, "%d\t" , m[i]);
-            simple_uart_putstring((const uint8_t *)buf);
-        }
-*/
-
 /*
-        // Place the read character in the payload, enable the radio and
-        // send the packet:
-        packet[0] = b ? '1' : '0';
-        NRF_RADIO->EVENTS_READY = 0U;
-        NRF_RADIO->TASKS_TXEN = 1;
-        while (NRF_RADIO->EVENTS_READY == 0U)
-        {
-        }
-        NRF_RADIO->TASKS_START = 1U;
-        NRF_RADIO->EVENTS_END = 0U;
-        while(NRF_RADIO->EVENTS_END == 0U)
-        {
-        }
-        NRF_RADIO->EVENTS_DISABLED = 0U;
-        // Disable radio
-        NRF_RADIO->TASKS_DISABLE = 1U;
-        while(NRF_RADIO->EVENTS_DISABLED == 0U)
-        {
+        // accelerometer:
+        for (int i=0; i<3; i++) {
+            DP_int(m[i]);
+            DP_str("\t");
         }
 */
-        nrf_gpio_pin_write(LED, b);
-        b = !b;
-        nrf_delay_ms(20);
+        // heading:
+        float heading = atan2(m[1], m[0]); // atan2(my, mx);
+        if(heading < 0)
+            heading += 2 * M_PI;
+        heading *= 180 / M_PI;
+        DP_int(heading);
+        DP_str("\t");
+
+        for (int i = 0; i < 90; i++)
+        {
+            if (i < heading / 4) // max = 360/4 = 90
+                DP_str("+");
+            else
+                DP_str(" ");
+        }
+        DP_str("|\r\n");
+
+        nrf_gpio_pin_write(LED, b = !b);
+        nrf_delay_ms(10);
     }
 }
 
-/**
- *@}
- **/
+void simple_uart_print_int(int i)
+{
+    char buf[12]; // 2**32 = 4G => sign + 10 numbers + '\0'
+    sprintf(buf, "%d" , i);
+    simple_uart_putstring((const uint8_t *)buf);
+}
