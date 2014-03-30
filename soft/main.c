ad386da6411f01e81d2a5d008bb1d86d99b36259
Cedric Honnet <honnet@telecom-paristech.org>
2014-03-29 17:00:47 -0700

    WIP: Send button and 4 accel events using ESB.

--- soft/main.c
+++ soft/main.c
@@ -6,6 +6,17 @@
 #include "nrf_delay.h"
 #include "MPU9150.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "nrf_esb.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+
 void simple_uart_print_int(int i);
 
 #define DEBUG_PRINT
@@ -22,55 +33,113 @@ void simple_uart_print_int(int i);
 #  define DP_str(x)
 #endif
 
+#define PIPE_NUMBER 0
+#define TX_PAYLOAD_LENGTH 1
+
+// Data and acknowledgement payloads
+static uint8_t my_tx_payload[TX_PAYLOAD_LENGTH];                ///< Payload to send to PRX.
+
 MPU9150 accelGyroMag;
-int16_t a[3], g[3], m[3];
+
+/////////////////////////////////////////////////////////////////////////////
 
 void init(void)
 {
     nrf_gpio_cfg_output(LED);
+    nrf_gpio_pin_set(LED);
+    nrf_gpio_cfg_input(BUTTON, NRF_GPIO_PIN_PULLDOWN);
+
     DP_cnf(RTS_PIN_NUMBER, TX_PIN_NUMBER, CTS_PIN_NUMBER, RX_PIN_NUMBER, false);
+    DP_str("Starting IMU:\n");
+
     accelGyroMag.initialize();
     DP_str( accelGyroMag.testConnection() ?
             "MPU9150 connection OK\r\n" :
             "MPU9150 connection failed\r\n" );
+
+    // Initialize ESB
+    nrf_esb_init(NRF_ESB_MODE_PTX);
+    nrf_esb_set_output_power(NRF_ESB_OUTPUT_POWER_4_DBM); // default 0
 }
+
+/////////////////////////////////////////////////////////////////////////////
+
+uint8_t poll_sensors(void) {
+    const int16_t threshold = 1 << 13; // 2**13 = 0.5g because [-2**15, 2**15] = [-2g, 2g]
+
+    static uint8_t old_bitmap = 0; // button(MSB), -y, y, -x, x (LSB)
+    uint8_t bitmap = 0;
+
+    int16_t x = accelGyroMag.getAccelerationX();
+    int16_t y = accelGyroMag.getAccelerationY();
+
+    if (x > threshold)       bitmap |= 1 << 0;
+    else if (x < -threshold) bitmap |= 1 << 1;
+
+    if (y > threshold)       bitmap |= 1 << 2;
+    else if (y < -threshold) bitmap |= 1 << 3;
+
+    if (nrf_gpio_pin_read(BUTTON)) bitmap |= 1 << 4;
+
+    DP_int(bitmap);
+    DP_str("\t");
+
+    DP_int(y);
+    DP_str("\t");
+    for (int i = 0; i < 64; i++) { // 16bit => 6bits (max = 63, min = -64)
+        if ( i < (abs(y) >> 9) ) DP_str("y");
+        else                     DP_str(" ");
+    }
+    DP_str("|\r\n");
+
+    if (old_bitmap && old_bitmap != bitmap)
+        return bitmap;
+    else
+        return 0;
+}
+
+/////////////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
-    char b = 0;
     init();
 
     while(1)
     {
-        accelGyroMag.getMotion9(&a[0], &a[1], &a[2], &g[0], &g[1], &g[2], &m[0], &m[1], &m[2]);
-/*
-        // accelerometer:
-        for (int i=0; i<3; i++) {
-            DP_int(m[i]);
-            DP_str("\t");
-        }
-*/
-        // heading:
-        float heading = atan2(m[1], m[0]); // atan2(my, mx);
-        if(heading < 0)
-            heading += 2 * M_PI;
-        heading *= 180 / M_PI;
-        DP_int(heading);
-        DP_str("\t");
+        my_tx_payload[0] = poll_sensors();
+        nrf_gpio_pin_write(LED, (my_tx_payload[0] != 0));
 
-        for (int i = 0; i < 90; i++)
-        {
-            if (i < heading / 4) // max = 360/4 = 90
-                DP_str("+");
-            else
-                DP_str(" ");
+        // Add packet into TX queue
+        if (my_tx_payload[0]) {
+            nrf_esb_add_packet_to_tx_fifo(PIPE_NUMBER, my_tx_payload, TX_PAYLOAD_LENGTH, NRF_ESB_PACKET_USE_ACK);
+            nrf_esb_enable();
         }
-        DP_str("|\r\n");
 
-        nrf_gpio_pin_write(LED, b = !b);
-        nrf_delay_ms(10);
+        nrf_delay_ms(30);
     }
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+// If the transmission failed, send a new packet.
+void nrf_esb_tx_failed(uint32_t tx_pipe){
+    (void)nrf_esb_add_packet_to_tx_fifo(PIPE_NUMBER, my_tx_payload, TX_PAYLOAD_LENGTH, NRF_ESB_PACKET_USE_ACK);
+}
+
+
+// Callbacks not needed in this example.
+void nrf_esb_tx_success(uint32_t tx_pipe, int32_t rssi){
+    // everything is in the main loop
+}
+void nrf_esb_rx_data_ready(uint32_t rx_pipe, int32_t rssi){
+    // nothing
+}
+void nrf_esb_disabled(void) {
+    // nothing
+}
+
+/////////////////////////////////////////////////////////////////////////////
 
 void simple_uart_print_int(int i)
 {
@@ -78,3 +147,4 @@ void simple_uart_print_int(int i)
     sprintf(buf, "%d" , i);
     simple_uart_putstring((const uint8_t *)buf);
 }
+
